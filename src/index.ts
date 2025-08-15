ef66293143d90e8a324c543d2aefa9b5eb37465b
cryptomafiaPB <jugad360@gmail.com>
2025-08-16 00:15:33 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,12 @@ const port = PORT || 5000
 connectDB();
 
 // config middlewares
-app.use(cors());
+app.use(cors(
+    {
+        origin: "http://localhost:3000",
+        credentials: true,
+    }
+));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
